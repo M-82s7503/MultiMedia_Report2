be526f7cb39ee1003491c408684b463047fd45d1
M-82s7503 <e165738@ie.u-ryukyu.ac.jp>
2018-11-27 12:20:56 +0900

    【ver0.08】「Python で  ３）まで完成」 ● C# の方は、Forms では 　画像を配列に変換するのが難しく、 　今回は実装を見送ることにした。 　ただし、「読み込み」と「表示」 　まではなんとか完了してる。

--- Python/ReinventingTheWheel/tone_curve.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-class ToneCurve:
-    def tone_curve(self, image, γ):
-        return image**(1/γ)
-
-
--- Python/edit_img.py
+++ Python/edit_img.py
@@ -1,6 +1,6 @@
 import sys
 import cv2
-from ReinventingTheWheel.tone_curve import ToneCurve
+import numpy as np
 
 def preview(image):
     cv2.imshow("preview",image) #画像出力 imshow(window_name, image)
@@ -14,24 +14,84 @@ image[:, :, (0,2)] = 0
 結果として、緑以外の色がなくなる（緑だけの画像になる）
 '''
 
+### トーンカーブ
+def tone_curve(img, γ):
+    return img**(1/γ)
+
+### αブレンディング
+def α_blending(imgs=[], αs=[]):
+    # or sum(αs) != 1
+    if len(imgs) < 2 or len(imgs) != len(αs):
+        return []  # ほんとはエラー処理すべき
+
+    # 0埋めの配列で初期化。
+    blend = np.zeros_like(imgs[0])
+    for img, α in zip(imgs, αs):
+        blend += α*img
+    return blend
+
+def adjust_imgs(size=(640,526), imgs=[]):
+    resized_imgs = []
+    for img in imgs:
+        resized_imgs.append( cv2.resize(img, size) / 255 )  # float（0~1）に変換する
+    return resized_imgs
+
+
+### 左右反転
+# 列を入れ替えることで実現。（スライスが使える）
+# https://note.nkmk.me/python-numpy-swap-select/
+def turn_vertical(img):
+    return img[:, ::-1]
+
+
+
 if __name__ == "__main__":
     args = sys.argv
     
     # 画像読み込み
-    image = cv2.imread(args[1]) / 255
+    print(len(args))
+    print(args[1])
 
+    image_edit = []
     # 画像を編集
-    tc = ToneCurve()
-    image_edit = tc.tone_curve(image, 2)
-    print(image)
-    print(image_edit)
+    # トーンカーブ
+    if args[1] == 'tc':
+        image = cv2.imread(args[2]) / 255  # float（0~1）に変換する
+        image_edit = tone_curve(image, 3)
+    
+    # αブレンディング
+    if args[1] == 'a':
+        print('αブレンディング を実行します！')
+        # 引数：  a 【画像数】 640 480  【img1_path】  【img2_path】  【img3_path】 ... 0.3  0.2  0.5
+        img_num = int(args[2])
+        size = ( int(args[3]),int(args[4]) )
+        imgs = []
+        for img_path in args[5:5+img_num]:
+            imgs.append( cv2.imread(img_path) )  # float（0~1）に変換する
 
+        imgs = adjust_imgs(size, imgs)
+
+        αs = [float(i) for i in args[ 5+img_num : ]]
+        print(αs)
+
+        image_edit = α_blending(imgs, αs)
+        print(image_edit)
+        if not image_edit.any():
+            sys.exit(-1)
+    
+    # 左右反転
+    if args[1] == 'turn':
+        image = cv2.imread(args[2]) / 255  # float（0~1）に変換する
+        image_edit = turn_vertical(image)
+
+#    print(image)
+#    print(image_edit)
 
     # 画像表示
     preview(image_edit)
     
     #画像保存 imwrite(filename, image)
-    cv2.imwrite("result/result.jpg",image_edit)
+    cv2.imwrite("result/result_{0}.png".format(args[1]), image_edit*255)
 
 
 
